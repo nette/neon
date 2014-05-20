5cb50ff25d17fc4d0f321050e99309aae1c77ad2
David Grudl <david@grudl.com>
2014-05-20 19:55:25 +0200

    Entity: added constructor

--- src/Neon/Entity.php
+++ src/Neon/Entity.php
@@ -15,6 +15,17 @@ use Nette;
  */
 class Entity extends \stdClass
 {
+	/** @var mixed */
 	public $value;
+
+	/** @var array */
 	public $attributes;
+
+
+	public function __construct($value = NULL, array $attrs = NULL)
+	{
+		$this->value = $value;
+		$this->attributes = (array) $attrs;
+	}
+
 }
