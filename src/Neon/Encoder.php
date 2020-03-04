3c3dcbc6bf6c80dc97b1fc4ba9a22ae67930fc0e
David Grudl <david@grudl.com>
2020-03-04 11:13:49 +0100

    Encoder: correctly encodes numeric strings

--- src/Neon/Encoder.php
+++ src/Neon/Encoder.php
@@ -67,8 +67,7 @@ final class Encoder
 
 		} elseif (
 			is_string($var)
-			&& !is_numeric($var)
-			&& !preg_match('~[\x00-\x1F]|^\d{4}|^(true|false|yes|no|on|off|null)$~Di', $var)
+			&& !preg_match('~[\x00-\x1F]|^[+-.]?\d|^(true|false|yes|no|on|off|null)$~Di', $var)
 			&& preg_match('~^' . Decoder::PATTERNS[1] . '$~Dx', $var) // 1 = literals
 		) {
 			return $var;
