970a570cbb5913c99c0e02cb06895532fbd6b518
David Grudl <david@grudl.com>
2023-09-27 14:05:28 +0200

    added Position to Exception

--- src/Neon/Exception.php
+++ src/Neon/Exception.php
@@ -15,4 +15,13 @@ namespace Nette\Neon;
  */
 class Exception extends \Exception
 {
+	public readonly ?Position $position;
+
+
+	public function __construct(string $message, ?Position $position = null, ?\Throwable $previous = null)
+	{
+		$message .= $position ? ' ' . $position : '';
+		$this->position = $position;
+		parent::__construct($message, 0, $previous);
+	}
 }
