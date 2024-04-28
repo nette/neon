22752ceaf3cd9cbd89c95c0a3e54ca3637ebbbb2
David Grudl <david@grudl.com>
2024-04-28 22:42:36 +0200

    TokenStream: renamed some methods

--- src/Neon/TokenStream.php
+++ src/Neon/TokenStream.php
@@ -15,7 +15,7 @@ use function in_array, str_replace, strlen, strrpos, substr, substr_count;
 /** @internal */
 final class TokenStream
 {
-	private int $pos = 0;
+	private int $index = 0;
 
 
 	public function __construct(
@@ -25,15 +25,15 @@ final class TokenStream
 	}
 
 
-	public function getPos(): int
+	public function getIndex(): int
 	{
-		return $this->pos;
+		return $this->index;
 	}
 
 
-	public function seek(int $position): void
+	public function seek(int $index): void
 	{
-		$this->pos = $position;
+		$this->index = $index;
 	}
 
 
@@ -44,31 +44,31 @@ final class TokenStream
 	}
 
 
-	public function isNext(int|string ...$types): bool
+	public function is(int|string ...$types): bool
 	{
-		while (in_array($this->tokens[$this->pos]->type ?? null, [Token::Comment, Token::Whitespace], strict: true)) {
-			$this->pos++;
+		while (in_array($this->tokens[$this->index]->type ?? null, [Token::Comment, Token::Whitespace], strict: true)) {
+			$this->index++;
 		}
 
 		return $types
-			? in_array($this->tokens[$this->pos]->type ?? null, $types, strict: true)
-			: isset($this->tokens[$this->pos]);
+			? in_array($this->tokens[$this->index]->type ?? null, $types, strict: true)
+			: isset($this->tokens[$this->index]);
 	}
 
 
-	public function consume(int|string ...$types): ?Token
+	public function tryConsume(int|string ...$types): ?Token
 	{
-		return $this->isNext(...$types)
-			? $this->tokens[$this->pos++]
+		return $this->is(...$types)
+			? $this->tokens[$this->index++]
 			: null;
 	}
 
 
 	public function getIndentation(): string
 	{
-		return in_array($this->tokens[$this->pos - 2]->type ?? null, [Token::Newline, null], strict: true)
-			&& ($this->tokens[$this->pos - 1]->type ?? null) === Token::Whitespace
-			? $this->tokens[$this->pos - 1]->value
+		return in_array($this->tokens[$this->index - 2]->type ?? null, [Token::Newline, null], strict: true)
+			&& ($this->tokens[$this->index - 1]->type ?? null) === Token::Whitespace
+			? $this->tokens[$this->index - 1]->value
 			: '';
 	}
 
@@ -76,7 +76,7 @@ final class TokenStream
 	/** @return never */
 	public function error(?string $message = null, ?int $pos = null): void
 	{
-		$pos ??= $this->pos;
+		$pos ??= $this->index;
 		$input = '';
 		foreach ($this->tokens as $i => $token) {
 			if ($i >= $pos) {
